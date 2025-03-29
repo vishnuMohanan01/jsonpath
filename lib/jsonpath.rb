15bd616a6f9a50d70fb7b793a7a8b26e9095ea0d
vishnuMohanan01 <iamvm.dev@gmail.com>
2025-03-29 20:07:24 +0530

    String interpolation

--- lib/jsonpath.rb
+++ lib/jsonpath.rb
@@ -115,11 +115,11 @@ class JsonPath
 
   def self.construct_path(table_row)
     if table_row[:index]
-      table_row[:root_key] + "[" + table_row[:index].to_s + "]"
+      "#{table_row[:root_key]}[#{table_row[:index].to_s}]"
     else
       table_row_key = table_row[:key].include?(".") ? "'#{table_row[:key]}'" : table_row[:key]
 
-      table_row[:root_key] + "." + table_row_key
+      "#{table_row[:root_key]}.#{table_row_key}"
     end
   end
 
